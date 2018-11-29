8ad636f81ab0f960875ed63378f9cc5f5827c33d
Joe Cooper <swelljoe@gmail.com>
2018-11-29 03:30:44 -0600

    Make it smarter about HTML pages and titles

--- pages.pl
+++ pages.pl
@@ -6,6 +6,7 @@ use 5.020;
 use Mojo::UserAgent;
 use Time::Piece;
 use HTML::WikiConverter;
+use List::Util qw(first);
 
 use File::Path qw(make_path);
 eval { make_path('content') };
@@ -17,7 +18,9 @@ if ($@) {
 # Convert HTML to markdown
 my $wc = new HTML::WikiConverter(dialect => 'Markdown');
 
-my @urls = ( 'download', 'docs', 'usermin', 'virtualmin', 'cloudmin', 'community', 'mirrors', 'devel', 'third', 'intro', 'support', 'demo', 'standard', 'lang', 'updates', 'changes', 'about', 'security' );
+my @urls;
+#my @urls = ( 'download', 'docs', 'usermin', 'virtualmin', 'cloudmin', 'community', 'mirrors', 'devel', 'intro', 'support', 'demo', 'lang', 'changes', 'about', 'security' );
+my @html_urls = ( 'third', 'standard', 'updates' );
 my @entries;
 for my $url ( @urls ) {
   my $ua  = Mojo::UserAgent->new;
@@ -27,6 +30,16 @@ for my $url ( @urls ) {
   my $md = $wc->html2wiki( $main );
   my $title = ucfirst($url);
   write_md($title, $md);
+}
+# Don't convert these to Markdown, because they have tables
+for my $html_url ( @html_urls ) {
+  my $ua = Mojo::UserAgent->new;
+  my $res = $ua->get("webmin.com/$html_url.html");
+
+  my $main = $res->res->dom->at('#main');
+  my $title = $res->res->dom->find('#main > h1')->map('text')->first;
+  $main =~ s/<h1>${title}<\/h1>//;
+  write_html($html_url, $title, $main);
 }
 
 sub write_md {
@@ -45,6 +58,28 @@ $md
 EOF
 
   my $filename = lc $title; 
+  $filename =~ s/\s/-/g;
+  $filename =~ s/[!,()'"\/]//g;
+  open(my $FILE, ">", "content/$filename.md");
+  print $FILE $out;
+  close $FILE;
+}
+
+sub write_html {
+  my ($filename, $title, $html) = @_;
+  my $date = '2017-10-02';
+  my $out = <<"EOF";
+---
+title: "$title"
+date: $date
+categories: []
+aliases: []
+toc: false
+draft: false
+---
+$html
+EOF
+
   $filename =~ s/\s/-/g;
   $filename =~ s/[!,()'"\/]//g;
   open(my $FILE, ">", "content/$filename.md");
